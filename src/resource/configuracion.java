8f16eb6832310d1c5fe352a1a3cc035c2768ffa6
maikel50 <palango4887@gmail.com>
2023-11-16 12:25:28 +0100

    cambios maikel config.properties

--- src/resource/configuracion.java
+++ src/resource/configuracion.java
@@ -1,44 +1,74 @@
 package resource;
 
+import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Properties;
 
+
+
+
 public class configuracion {
-
-private Properties properties;
-
-public configuracion () {
-	this.properties=properties;
-	cargarConfiguracion();
-}
-
-private  void cargarConfiguracion() {
-	FileInputStream input;
+	public static void main(String[] args) {
+	Properties configuracion = new Properties();
+	
 	try {
-		input = new FileInputStream("src/resources/config.properties");
-		properties.load(input);
-	}catch(IOException e) {
-		e.printStackTrace();
+		configuracion.load(new FileReader("src/resource/config.properties"));
+		tiempoCiudad(configuracion);
+	}catch(Exception e) {
+		e.printStackTrace();	}
+	
 	}
-}
 
-public String obtenerValor(String clave) {
-    return properties.getProperty(clave);
-}
+	
 
-public void establecerValor(String clave, String valor) {
-    properties.setProperty(clave, valor);
-    guardarConfiguracion();
-}
+	private static void tiempoCiudad(Properties configuracion) {
+		 String ciudad = configuracion.getProperty("nombre");
+	     String web = configuracion.getProperty("web");
 
-private void guardarConfiguracion() {
-    try (FileOutputStream output = new FileOutputStream("src/resources/config.properties")) {
-        properties.store(output, null);
-    } catch (IOException e) {
-        e.printStackTrace();
-    }
-}
+	     // Realizar la solicitud a la dirección web y obtener la respuesta JSON
+	     String respuestaJson = obtenerRespuestaWeb(web);
+	     //Obtener informacion del tiempo
+	    
+	}
 
+
+
+	private static String obtenerRespuestaWeb(String web) {
+		StringBuilder respuesta = new StringBuilder();
+		BufferedReader leer;
+	    try {
+	        // Crear URL
+	        URL url = new URL(web);
+
+	        // Abrir conexión HTTP
+	        HttpURLConnection conexion = (HttpURLConnection) url.openConnection();
+
+	        // Configurar la solicitud
+	        conexion.setRequestMethod("GET");
+
+	        // Obtener la respuesta
+	        try  {
+	        	leer =  new BufferedReader(new InputStreamReader(conexion.getInputStream()));
+	            String linea;
+	            while ((linea = leer.readLine()) != null) {
+	                respuesta.append(linea);
+	            }
+
+	        // Cerrar la conexión
+	        conexion.disconnect();
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	    }
+	}
+	    catch(Exception e) {
+	    	e.printStackTrace();
+	    }
+	    return respuesta.toString();
+}
 }
